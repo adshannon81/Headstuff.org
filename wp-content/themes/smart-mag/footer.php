37315ae55261f8760d009a97269d6ba27d26e92f
adshannon81 <ad.shannon@gmail.com>
2015-03-19 10:26:19 +0000

    Update footer.php

--- wp-content/themes/smart-mag/footer.php
+++ wp-content/themes/smart-mag/footer.php
@@ -1,39 +1,31 @@
-
-	<footer class="main-footer">
-	
-
-		<div class="wrap">
-
-			<ul class="widgets row cf">
-				<?php dynamic_sidebar('main-footer'); ?>
-			</ul>
-		
-		</div>
-	
-
-
-		<div class="lower-foot">
-			<div class="wrap">
-		
-			<div class="widgets">
-				<?php dynamic_sidebar('lower-footer'); ?>
-			</div>
-
-		
-			</div>
-		</div>		
-
-	<div class="lower-foot">
-		<div class="wrap">
-			<span class="pull-left">
-				<span><a href="http://qwerty.ie/">Web Agency</a> - Qwerty.ie</span>
-			</span>
-			<span class="pull-right">
-				<span>Copyright © 2014 <a href="http://www.headstuff.org">Headstuff.org</a>. All Rights Reserved.</span>
-			</span>
-		</div>
-	</div>
-	</footer>
+<footer class="main-footer">
+<div class="wrap">
+<ul class="widgets row cf">
+<li class="widget col-4 widget_nav_menu"><h3 class="widgettitle">Contact</h3><div class="menu-footer-menu-container"><ul id="menu-footer-menu" class="menu"><li id="menu-item-7480" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-7480"><a href="http://headstuff.org/contact-us">We’re open to submissions!</a></li>
+<li id="menu-item-1775" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1775"><a href="mailto:editor@headstuff.org?subject=Message%20from%20HeadStuff%20link">editor@headstuff.org</a></li>
+<li id="menu-item-1776" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1776"><a href="http://www.facebook.com/theheadstuff">Facebook</a></li>
+<li id="menu-item-1777" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1777"><a href="http://twitter.com/thisheadstuff">Twitter</a></li>
+<li id="menu-item-1778" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1778"><a href="http://astore.amazon.co.uk/head0a-21">Bookstore</a></li>
+</ul></div></li><li class="widget col-4 widget_text"><h3 class="widgettitle">About Headstuff</h3> <div class="textwidget">HeadStuff.org is a collaborative hub for the creative and the curious. We all spend so much of our time clicking through reams of content and sometimes not reading anything of interest at all. HeadStuff is all about putting buckets of interesting stuff in one place. So it's easy for you to find and enjoy high quality content from our wonderful contributors. We get up every morning to make your daily journey through the internet more interesting and productive. We're all about the quality, and support good writing and reading. </div>
+</li>
+<li class="widget col-4 bunyad-about"> <h3 class="widgettitle">Arts and Culture Blog of the Year 2014</h3>
+<div class="about-widget">
+<p><a href="http://www.digitalmedia.ie/"><img src="http://www.headstuff.org/wp-content/uploads/2015/01/DMA_Finalists-Button.png" width="150px" height="auto" style="margin-top:5px" class="no-display appear"></a><a href="http://www.dublincityofliterature.ie/"><img src="http://www.headstuff.org/wp-content/uploads/2015/02/UNESCO-City-of-Literature.png" width="120px" height="auto" style="margin-left:20px" class="no-display appear"></a><a href="http://www.blogawardsireland.com/category/winners-2014/"><img src="http://www.blogawardsireland.com/wp-content/uploads/2014/05/blog_buttons_WINNER.png" width="150px" height="150px" style="margin-top:5px" class="no-display appear"></a></p>
+</div>
+</li>
+</ul>
+</div>
+<div class="lower-foot">
+<div class="wrap">
+<span class="pull-left">
+<span><a href="http://qwerty.ie/">Web Agency</a> - Qwerty.ie</span>
+</span>
+<span class="pull-right">
+<span>Copyright © 2014 <a href="http://www.headstuff.org">Headstuff.org</a>. All Rights Reserved.</span>
+</span>
+</div>
+</div>
+</footer>
 	
 </div> <!-- .main-wrap -->
 
